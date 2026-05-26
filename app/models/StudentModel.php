83a2b23cff18db3a2a21ae3ebe3825770a4d9f20
phat2005 <doctorgamer1156@gmail.com>
2026-05-27 00:37:50 +0700

    add Controller + connect DB add SinhVien Model :: getAll()

--- app/models/StudentModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class StudentModel
-{
-    public $name = "Nguyen Hung Phat";
-    public $mssv = "0021068";
-}
-
-?>
--- public/index.php
+++ public/index.php
@@ -1,11 +1,7 @@
 <?php
 
-require_once '../app/core/App.php';
-require_once '../app/middleware.php';
-
-$middleware = new Middleware();
-$middleware->checklogin();
-
-$app = new App();
-
+    require_once '../app/middleware.php';
+    $middleware = new Middleware();
+    $middleware->checklogin();
+    $app = new App();
 ?>
